3ad5678b1c05ddd472b96ab8c13c1a057236109c
abulubabah <rezamalikdrive@gmail.com>
2020-02-17 09:29:58 +0700

    code tiding, techer_entity added

--- dbmysql/dblearning.sql
+++ dbmysql/dblearning.sql
@@ -22,6 +22,8 @@ CREATE TABLE IF NOT EXISTS `class_entity` (
   `cls_code` varchar(10) NOT NULL DEFAULT '',
   `cls_name` varchar(50) NOT NULL DEFAULT '',
   `cls_desc` text NOT NULL,
+  `created_at` date NOT NULL,
+  `creatde_by` varchar(10) NOT NULL DEFAULT '',
   PRIMARY KEY (`cls_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for class entity';
 
@@ -33,6 +35,8 @@ CREATE TABLE IF NOT EXISTS `institution_entity` (
   `instt_code` varchar(10) NOT NULL DEFAULT '',
   `instt_name` varchar(50) NOT NULL DEFAULT '',
   `instt_desc` text NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(10) NOT NULL DEFAULT '',
   PRIMARY KEY (`instt_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for institution entity';
 
@@ -42,15 +46,37 @@ CREATE TABLE IF NOT EXISTS `institution_entity` (
 CREATE TABLE IF NOT EXISTS `student_entity` (
   `std_id` int(10) NOT NULL,
   `std_name` varchar(50) NOT NULL,
+  `std_code` varchar(10) NOT NULL DEFAULT '1',
   `std_sx` int(1) NOT NULL DEFAULT '1',
   `std_pob` varchar(50) NOT NULL,
   `std_dob` datetime NOT NULL,
   `std_addrs` text NOT NULL,
   `username` varchar(10) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(10) NOT NULL DEFAULT '',
   PRIMARY KEY (`std_id`),
   KEY `username` (`username`),
   CONSTRAINT `FK_student_entity_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for students entities';
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table learning.teacher_entity
+CREATE TABLE IF NOT EXISTS `teacher_entity` (
+  `tchr_id` int(10) NOT NULL,
+  `tchr_code` varchar(10) NOT NULL DEFAULT '',
+  `tchr_name` varchar(50) NOT NULL DEFAULT '',
+  `tchr_sx` int(1) NOT NULL DEFAULT '1',
+  `tchr_pob` varchar(50) NOT NULL,
+  `tchr_dob` date NOT NULL,
+  `tchr_addrs` text NOT NULL,
+  `username` varchar(10) NOT NULL DEFAULT '',
+  `created_at` date NOT NULL,
+  `created_by` varchar(10) NOT NULL DEFAULT '',
+  PRIMARY KEY (`tchr_id`),
+  KEY `username` (`username`),
+  CONSTRAINT `FK_teacher_entity_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for techer entity';
 
 -- Data exporting was unselected.
 
